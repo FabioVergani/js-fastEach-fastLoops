b3721766ab6e2631354a4b2bae82aae34d858b40
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-23 13:09:23 +0200

    Update consume.js

--- consume.js
+++ consume.js
@@ -1,3 +1,6 @@
+function consume(m,cb){var e,f=cb;while((e=m.shift())){f(e);}}
+
+
 function consume(m,callback){var e,f=callback;while(e=m.shift()){f(e);}/*;*/}
 consume([1,2,3,4,5],function(x){console.log(x);});
 
